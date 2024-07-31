55d5842af44fa307f85504149e9ac089a75ce5bf
ahmedabdelrshed <104847521+ahmedabdelrshed@users.noreply.github.com>
2024-07-31 07:57:20 +0300

    update todo modal

--- src/pages/TodoList.tsx
+++ src/pages/TodoList.tsx
@@ -1,11 +1,15 @@
 import Button from "../ui/Button";
 import { ITodo } from "../interfaces";
 import useAuthenticatedQuery from "../hooks/useAuthenticatedQuery";
+import Modal from "../ui/Modal";
+import Input from "../ui/Input";
+import { useState } from "react";
 
 const TodoList = () => {
   const userDataString = localStorage.getItem("userData");
   const userData = userDataString ? JSON.parse(userDataString) : null;
-  // fetch data using custom query hook 
+  const [isEditModal, setIsEditModal] = useState(false);
+  // fetch data using custom query hook
   const { isLoading, data } = useAuthenticatedQuery({
     queryKey: ["todos"],
     url: "/users/me?populate=todos",
@@ -16,6 +20,8 @@ const TodoList = () => {
     },
   });
   if (isLoading) return "Loading...";
+  // Handlers
+  const toggleEditModal = () => setIsEditModal(!isEditModal);
   return (
     <div className="space-y-2">
       {data.todos.length ? (
@@ -26,14 +32,25 @@ const TodoList = () => {
           >
             <p className="w-full font-semibold">{todo.title}</p>
             <div className="flex items-center justify-end w-full space-x-3">
-              <Button>Edit</Button>
-              <Button>Remove</Button>
+              <Button onClick={toggleEditModal}>Edit</Button>
+              <Button className="bg-red-700">Remove</Button>
             </div>
           </div>
         ))
       ) : (
         <h2>You don't have todos Yet !!!</h2>
       )}
+      <Modal
+        isOpen={isEditModal}
+        title="Edit Todo"
+        closeModal={toggleEditModal}
+      >
+        <Input name="title" value={"Edit Todo"}/>
+        <div className="flex items-center justify-between mt-4 space-x-5">
+          <Button >Update</Button>
+          <Button onClick={toggleEditModal} className="bg-red-700">Cancel</Button>
+        </div>
+      </Modal>
     </div>
   );
 };
--- src/ui/Button.tsx
+++ src/ui/Button.tsx
@@ -4,13 +4,20 @@ interface IProps extends ButtonHTMLAttributes<HTMLButtonElement> {
   children: ReactNode;
   width?: "w-full" | "w-fit";
   isLoading?: boolean;
+  className?: string;
 }
 
-const Button = ({ children, width = "w-full", isLoading, ...rest }: IProps) => {
+const Button = ({
+  children,
+  width = "w-full",
+  className,
+  isLoading,
+  ...rest
+}: IProps) => {
   return (
     <button
       disabled={isLoading}
-      className={`${width} disabled:hover:bg-blue-400, disabled:cursor-not-allowed disabled:bg-blue-400 transition bg-blue-600 p-2  rounded-lg font-medium text-white`}
+      className={`${width} disabled:hover:bg-blue-400, disabled:cursor-not-allowed disabled:bg-blue-400 transition bg-blue-600 p-2  rounded-lg font-medium text-white ${className}`}
       {...rest}
     >
       {children}

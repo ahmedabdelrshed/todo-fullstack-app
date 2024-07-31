dd1c9d9755f6b7eee8f788a96c0dbf2a8e7e6404
ahmedabdelrshed <104847521+ahmedabdelrshed@users.noreply.github.com>
2024-07-31 21:01:14 +0300

    create new todo

--- src/pages/TodoList.tsx
+++ src/pages/TodoList.tsx
@@ -9,12 +9,14 @@ import axiosInstance from "../config/axios.config";
 import { validateTodo } from "../validation";
 import ErrorMassage from "../errors/ErrorMassage";
 import toast from "react-hot-toast";
+import TodoListSkeleton from "../ui/TodoListSkeleton";
 
 const TodoList = () => {
   const userDataString = localStorage.getItem("userData");
   const userData = userDataString ? JSON.parse(userDataString) : null;
   const [isEditModal, setIsEditModal] = useState(false);
   const [isDeleteModal, setIsDeleteModal] = useState(false);
+  const [isAddModal, setIsAddModal] = useState(false);
   const [errorEdit, setErrorEdit] = useState({
     title: "",
     description: "",
@@ -25,6 +27,7 @@ const TodoList = () => {
     title: "",
   };
   const [editTodo, setEditTodo] = useState<ITodo>(defaultTodo);
+  const [addTodo, setAddTodo] = useState({ title: "", description: "" });
   // fetch data using custom query hook
   const { isLoading, data } = useAuthenticatedQuery({
     queryKey: ["TodoList", `${editTodo.id}`],
@@ -35,10 +38,11 @@ const TodoList = () => {
       },
     },
   });
-  if (isLoading) return "Loading...";
+  if (isLoading) return <TodoListSkeleton />;
   // Handlers
   const toggleEditModal = () => setIsEditModal(!isEditModal);
   const toggleDeleteModal = () => setIsDeleteModal(!isDeleteModal);
+  const toggleAddModal = () => setIsAddModal(!isAddModal);
   const onEditTodo = (todo: ITodo) => {
     setEditTodo(todo);
     toggleEditModal();
@@ -48,11 +52,23 @@ const TodoList = () => {
     toggleEditModal();
     setErrorEdit({ title: "", description: "" });
   };
+  const onCloseAddModal = () => {
+    setAddTodo({ title: "", description: "" });
+    toggleAddModal();
+    setErrorEdit({ title: "", description: "" });
+  };
   const onChangeEditTodo = (
     event: ChangeEvent<HTMLInputElement> | ChangeEvent<HTMLTextAreaElement>
   ) => {
     const { value, name } = event.target;
     setEditTodo({ ...editTodo, [name]: value });
+    setErrorEdit({ ...errorEdit, [name]: "" });
+  };
+  const onChangeAddTodo = (
+    event: ChangeEvent<HTMLInputElement> | ChangeEvent<HTMLTextAreaElement>
+  ) => {
+    const { value, name } = event.target;
+    setAddTodo({ ...addTodo, [name]: value });
     setErrorEdit({ ...errorEdit, [name]: "" });
   };
   const onDeleteTodo = (id: number) => {
@@ -98,8 +114,39 @@ const TodoList = () => {
       console.log(error);
     }
   };
+  const onSubmitAddTodo = async (event: FormEvent<HTMLFormElement>) => {
+    event.preventDefault();
+    const { title, description } = addTodo;
+    const errors = validateTodo(addTodo.title, addTodo.description);
+    const checkErrorMsg = Object.values(errors).every((error) => error === "");
+    if (!checkErrorMsg) {
+      setErrorEdit(errors);
+      return;
+    }
+    try {
+      axiosInstance.post(
+        "/todos",
+        { data: { title, description } },
+        {
+          headers: {
+            Authorization: `Bearer ${userData.jwt}`,
+          },
+        }
+      );
+      onCloseAddModal();
+      toast.success("Todo Added successfully");
+    } catch (error) {
+      console.log(error);
+    }
+  };
   return (
     <div className="space-y-2">
+      <div className=" mx-auto my-10 flex justify-between items-center">
+        <h2 className="text-2xl font-semibold">Your Todos</h2>
+        <Button width="w-fit" onClick={toggleAddModal}>
+          Create New Todo
+        </Button>
+      </div>
       {data.todos.length ? (
         data.todos.map((todo: ITodo) => (
           <div
@@ -123,6 +170,38 @@ const TodoList = () => {
       ) : (
         <h2>You don't have todos Yet !!!</h2>
       )}
+      <Modal
+        isOpen={isAddModal}
+        title="Create New Todo"
+        closeModal={toggleAddModal}
+      >
+        <form onSubmit={onSubmitAddTodo}>
+          <Input
+            name="title"
+            value={addTodo.title}
+            onChange={onChangeAddTodo}
+          />
+          {errorEdit.title ? <ErrorMassage msg={errorEdit.title} /> : null}
+          <Textarea
+            value={addTodo.description}
+            name="description"
+            onChange={onChangeAddTodo}
+          />
+          {errorEdit.description ? (
+            <ErrorMassage msg={errorEdit.description} />
+          ) : null}
+          <div className="flex items-center justify-between mt-4 space-x-5">
+            <Button>Create</Button>
+            <Button
+              onClick={onCloseAddModal}
+              type="button"
+              className="bg-[#C0344D]"
+            >
+              Cancel
+            </Button>
+          </div>
+        </form>
+      </Modal>
       <Modal
         isOpen={isEditModal}
         title="Edit Todo"
---
+++ src/ui/TodoListSkeleton.tsx
@@ -0,0 +1,71 @@
+
+const TodoListSkeleton = () => {
+  return (
+   
+<div className="space-y-3">
+<div role="status" className="max-w-md mx-auto space-y-4  rounded shadow animate-pulse dark:divide-gray-700 md:p-6 dark:border-gray-700">
+    <div className="flex items-center justify-between ">
+        <div>
+            <div className="h-3 bg-gray-300 rounded-full dark:bg-gray-600 w-36 "></div>
+           
+        </div>
+        <div className="flex items-center justify-between space-x-2">
+        <div className="h-8 bg-gray-300 rounded-md dark:bg-gray-700 w-16"></div>
+        <div className="h-8 bg-gray-300 rounded-md dark:bg-gray-700 w-16"></div>
+        </div>
+    </div>
+</div>
+<div role="status" className="max-w-md mx-auto space-y-4  rounded shadow animate-pulse dark:divide-gray-700 md:p-6 dark:border-gray-700">
+    <div className="flex items-center justify-between ">
+        <div>
+            <div className="h-3 bg-gray-300 rounded-full dark:bg-gray-600 w-36 "></div>
+           
+        </div>
+        <div className="flex items-center justify-between space-x-2">
+        <div className="h-8 bg-gray-300 rounded-md dark:bg-gray-700 w-16"></div>
+        <div className="h-8 bg-gray-300 rounded-md dark:bg-gray-700 w-16"></div>
+        </div>
+    </div>
+</div>
+<div role="status" className="max-w-md mx-auto space-y-4  rounded shadow animate-pulse dark:divide-gray-700 md:p-6 dark:border-gray-700">
+    <div className="flex items-center justify-between ">
+        <div>
+            <div className="h-3 bg-gray-300 rounded-full dark:bg-gray-600 w-36 "></div>
+           
+        </div>
+        <div className="flex items-center justify-between space-x-2">
+        <div className="h-8 bg-gray-300 rounded-md dark:bg-gray-700 w-16"></div>
+        <div className="h-8 bg-gray-300 rounded-md dark:bg-gray-700 w-16"></div>
+        </div>
+    </div>
+</div>
+<div role="status" className="max-w-md mx-auto space-y-4  rounded shadow animate-pulse dark:divide-gray-700 md:p-6 dark:border-gray-700">
+    <div className="flex items-center justify-between ">
+        <div>
+            <div className="h-3 bg-gray-300 rounded-full dark:bg-gray-600 w-36 "></div>
+           
+        </div>
+        <div className="flex items-center justify-between space-x-2">
+        <div className="h-8 bg-gray-300 rounded-md dark:bg-gray-700 w-16"></div>
+        <div className="h-8 bg-gray-300 rounded-md dark:bg-gray-700 w-16"></div>
+        </div>
+    </div>
+</div>
+<div role="status" className="max-w-md mx-auto space-y-4  rounded shadow animate-pulse dark:divide-gray-700 md:p-6 dark:border-gray-700">
+    <div className="flex items-center justify-between ">
+        <div>
+            <div className="h-3 bg-gray-300 rounded-full dark:bg-gray-600 w-36 "></div>
+           
+        </div>
+        <div className="flex items-center justify-between space-x-2">
+        <div className="h-8 bg-gray-300 rounded-md dark:bg-gray-700 w-16"></div>
+        <div className="h-8 bg-gray-300 rounded-md dark:bg-gray-700 w-16"></div>
+        </div>
+    </div>
+</div>
+</div>
+
+  )
+}
+
+export default TodoListSkeleton
